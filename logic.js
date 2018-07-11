f143f6b373d1bf98af9ac0053ebaf3bdcf9c7bb5
Ons <onsmariposa-nana@hotmail.com>
2018-07-11 09:40:29 +0300

    added delete function

--- logic.js
+++ logic.js
@@ -7,31 +7,38 @@ var todoFunctions = {
     // You do not need to understand the implementation of this function.
     generateId: (function() {
       var idCounter = 0;
-  
+
       function incrementCounter() {
         return (idCounter += 1);
       }
-  
+
       return incrementCounter;
     })(),
-    
-    //cloneArrayOfObjects will create a copy of the todos array 
+
+    //cloneArrayOfObjects will create a copy of the todos array
     //changes to the new array don't affect the original
     cloneArrayOfObjects: function(todos) {
       return todos.map(function(todo){
         return JSON.parse(JSON.stringify(todo));
       });
     },
-    
-    addTodo: function(todos, newTodo) {
+
+    addTaddTodoodo: function(todos, newTodo) {
       // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
       // returns a new array, it should contain todos with the newTodo added to the end.
       // add an id to the newTodo. You can use the generateId function to create an id.
       // hint: array.concat
+
     },
     deleteTodo: function(todos, idToDelete) {
       // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
       // return a new array, this should not contain any todo with an id of idToDelete
+       var cloned = todoFunctions.cloneArrayOfObjects(todos);
+       return cloned.filter(function(item){
+       return item.id != idToDelete;
+      });
+
+
       // hint: array.filter
     },
     markTodo: function(todos, idToMark) {
@@ -47,11 +54,11 @@ var todoFunctions = {
       // hint: array.slice, array.sort
     },
   };
-  
-  
+
+
   // Why is this if statement necessary?
   // The answer has something to do with needing to run code both in the browser and in Node.js
-  // See this article for more details: 
+  // See this article for more details:
   // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
   if (typeof module !== 'undefined') {
     module.exports = todoFunctions;
--- test.js
+++ test.js
@@ -5,3 +5,27 @@ test('Example test', function(t) {
   t.pass();
   t.end();
 });
+
+
+test('deleteToDo list', function(assert) {
+  var actual = logic.deleteTodo
+    ([
+      {
+        id: 0,
+        description: 'read a chapter',
+        done: true,
+      },{
+        id: 1,
+        description: 'play a game',
+        done: false,
+      }],1);
+
+    var expected =[{
+  id: 0,
+  description: 'read a chapter',
+  done: true,
+}]
+
+  assert.deepEqual(actual, expected, 'should delete a task');
+  assert.end();
+});
